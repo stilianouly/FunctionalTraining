5d17e701362f1ca78ec56dc0f439dd245ce4f791
Stilianos Menelaou <stilianos.menelaou@rea-group.com>
2017-02-22 14:53:13 +1100

    Revert OptionalExercises & FoldExercises mistakes
    What was I thinking?

--- src/test/scala/com/rea/typesafety/OptionalExercises2Spec.scala
+++ src/test/scala/com/rea/typesafety/OptionalExercises2Spec.scala
@@ -8,35 +8,35 @@ class OptionalExercises2Spec extends Specification {
   "Get Environment for Host" should {
 
     "be prod for host1" in {
-      getEnvForHost("host1") === Some("prod")
+      getEnvForHost("host1") === "prod"
     }
 
     "be test for host2" in {
-      getEnvForHost("host2") === Some("test")
+      getEnvForHost("host2") === "test"
     }
 
     "be couldn't resolve for host3 and host 4" in {
-      getEnvForHost("host3") === Some("couldn't resolve")
-      getEnvForHost("host4") === Some("couldn't resolve")
+      getEnvForHost("host3") === "couldn't resolve"
+      getEnvForHost("host4") === "couldn't resolve"
     }
   }
 
   "Connect to ReaHostsOnly" should {
 
     "connected to rea.com for host1" in {
-      connectToReaHostsOnly("host1") === Some("connected to rea.com")
+      connectToReaHostsOnly("host1") === "connected to rea.com"
     }
 
     "connected to test.rea.com for host2" in {
-      connectToReaHostsOnly("host2") === Some("connected to test.rea.com")
+      connectToReaHostsOnly("host2") === "connected to test.rea.com"
     }
 
     "not be connected to netflix.com for host3" in {
-      connectToReaHostsOnly("host3") === Some("not connected")
+      connectToReaHostsOnly("host3") === "not connected"
     }
 
     "not be connected to unknown for host4" in {
-      connectToReaHostsOnly("host4") === Some("not connected")
+      connectToReaHostsOnly("host4") === "not connected"
     }
   }
 }
